32aa835f83c4f5356fe7a4504d7d3cd014059831
Rozan Skaik <43583602+RozanSkaik@users.noreply.github.com>
2026-03-05 00:06:20 +0200

    - add claude integration, solve animation issue - open custom dialog on second tab view

---
+++ KiddoPlay/Views/CustomViews/CustomDialog.swift
@@ -0,0 +1,18 @@
+//
+//  CustomDialog.swift
+//  KiddoPlay
+//
+//  Created by Rozan Skaik on 27/02/2026.
+//
+
+import SwiftUI
+
+struct CustomDialog: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomDialog()
+}
